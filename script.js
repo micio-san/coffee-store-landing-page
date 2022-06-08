af361303b7c5f79d468854891a04caf1f8d629ff
micio-san <106929734+micio-san@users.noreply.github.com>
2022-06-08 02:48:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+const mover = document.querySelector('.mover');
+const sliders = document.querySelectorAll('.moved');
+const height = sliders[0].clientHeight;
+console.log(sliders)
+let idx = 0;
+
+function moveSlides() {
+    idx++;
+    mover.style.transform = `translateY(-${height * idx}px)`;
+    mover.style.transition = 0.5 + 's';
+    if (idx === sliders.length) {
+        idx = 0
+        mover.style.transform = `translateY(-${height * idx}px)`;
+        mover.style.transition ='none';
+    }
+    console.log(idx)
+}
+
+setInterval(moveSlides,3000)
+
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+
+burger.addEventListener('click', () => {
+    menu.classList.toggle('active');
+})
+
+document.addEventListener('scroll', () => {
+    menu.classList.remove('active');
+})
+
+const popUp = document.querySelector('.pop-up');
+const background = document.querySelector('.back-ground');
+const body= document.querySelector('body')
+const options = {
+    threshold: 1,
+}
+const neko = new IntersectionObserver(function (els) {
+    els.forEach(el => {
+        if (el.isIntersecting) {
+            el.target.classList.add('active');
+            background.classList.add('active')
+            close();
+            body.style.overflowY='hidden'
+        }
+    })
+
+
+}, options)
+
+neko.observe(popUp)
+
+function close() {
+    const close = popUp.querySelector('.close-popUp');
+    close.addEventListener('click', () => {
+        popUp.classList.remove('active')
+        neko.unobserve(popUp);
+        background.classList.remove('active')
+        body.style.overflowY='scroll'
+    })
+}
+
+window.addEventListener('scroll', nonscroll);
+
+function nonscroll() {
+
+    console.log(window.screenY)
+}
+nonscroll()
